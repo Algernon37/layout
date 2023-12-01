85cc6a8ca7cbaa483beb4247007d3c28f4a686cb
Ivan Esin <ivan.esin.78@mail.ru>
2023-12-01 18:28:02 +0400

    fifth commit

--- GrowBE/JS/script.js
+++ GrowBE/JS/script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
             const readMoreLink = document.createElement("a");
             readMoreLink.href = "#";
             readMoreLink.textContent = "Read More...";
+            readMoreLink.style.color = "black";
             readMoreLink.addEventListener("click", function(event) {
                 event.preventDefault();
                 textElement.textContent = fullText;
@@ -32,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function() {
             adaptiveHeight: true,
             slidesToShow: 2,
             slidesToScroll: 1,
-            speed: 600,
+            speed: 700,
             easing: 'easing',
             infinite: true,
             initialSlide: 0,
@@ -51,6 +52,37 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     
         $('.slider__item').on('click', function() {
+            var index = $(this).data('slick-index');
+            slider.slick('slickGoTo', index);
+        });
+    });
+
+    $(document).ready(function(){
+        var slider = $('.slider__news').slick({
+            arrows: false,
+            dots: true,
+            adaptiveHeight: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            speed: 1000,
+            easing: 'easing',
+            infinite: true,
+            initialSlide: 0,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            pauseOnFocus: true,
+            pauseOnHover: true,
+            pauseOnDotsHover: true,
+            draggable: true,
+            swipe: true,
+            touchThreshold: 5,
+            touchMove: true,
+            waitForAnimate: true,
+            centerMode: true,
+            variableWidth: true
+        });
+    
+        $('.slider-item').on('click', function() {
             var index = $(this).data('slick-index');
             slider.slick('slickGoTo', index);
         });
