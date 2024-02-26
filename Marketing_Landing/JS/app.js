4b5a2e6b63c58b6221b6592a473faf55f6db7be0
Ivan Esin <ivan.esin.78@mail.ru>
2024-02-26 21:12:45 +0400

    add a new functions

--- Marketing_Landing/JS/app.js
+++ Marketing_Landing/JS/app.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     const burgerMenu = document.getElementById("burgerMenu");
     const nav = document.getElementById("nav");
-    const cardButton = document.getElementById("cartButton");
-    const productCardLink = document.getElementById('productCardLink');
+    const productCardLinks = document.querySelectorAll('.products__cards');
 
     // При клике на кнопку бургера, добавляем или удаляем класс "active" для меню
     burgerMenu.addEventListener("click", function () {
         nav.classList.toggle("active");
     });
+
     window.onload = function () {
         window.scrollTo(0, 0);
     };
 
-    productCardLink.addEventListener('click', function () {
-        window.location.href = '#';
-    });
-
-    cardButton.addEventListener("click", function (e) {
-        e.stopPropagation();
-        window.location.href = '#';
+    productCardLinks.forEach(function (card) {
+        card.addEventListener('click', function () {
+            window.location.href = '../frames/product.html';
+        });
     });
 
     $(document).ready(function () {
@@ -44,6 +41,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         }
     });
+
 })
 
 $(document).ready(function () {
