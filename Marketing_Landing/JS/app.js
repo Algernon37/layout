0acc774b339e621a7b34a16e81c43e3fc7168415
Ivan Esin <ivan.esin.78@mail.ru>
2024-04-03 20:47:11 +0400

    Do my homework

--- Marketing_Landing/JS/app.js
+++ Marketing_Landing/JS/app.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const burgerMenu = document.getElementById("burgerMenu");
     const nav = document.getElementById("nav");
-    const productCardLinks = document.querySelectorAll('.products__card');
     
-
     // При клике на кнопку бургера, добавляем или удаляем класс "active" для меню
     burgerMenu.addEventListener("click", function () {
         nav.classList.toggle("active");
@@ -13,11 +11,6 @@ document.addEventListener("DOMContentLoaded", function () {
         window.scrollTo(0, 0);
     };
 
-    productCardLinks.forEach(function (card) {
-        card.addEventListener('click', function () {
-            window.location.href = '../frames/product.html';
-        });
-    });
 
     $(document).ready(function () {
         let header = $("#header");
@@ -53,15 +46,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
-$(document).ready(function () {
-    $('.slider').slick({
-        arrows: true,
-        dots: false,
-        slidesToShow: 1,
-        autoplay: false,
-        speed: 1000,
-        autoplaySpeed: false,
-    });
-});
 
 
